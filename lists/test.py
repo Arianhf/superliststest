a332d00158d225cf46a14a623539121a54388def
Arian Hedayati Far <arian.hedayatifar@gmail.com>
2017-04-23 12:52:05 +0430

    Model for list Items annd associated migration

--- lists/test.py
+++ lists/test.py
@@ -5,13 +5,9 @@ from django.http import HttpRequest
 
 
 from lists.views import home_page
+from lists.models import Item
 
 class HomePageTest(TestCase):
-
-	def check_for_row_in_list_table(self, row_text):
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn(row_text, [row.text for row in rows])
 
 	def test_uses_home_template(self):
 		response = self.client.get('/')
@@ -22,3 +18,23 @@ class HomePageTest(TestCase):
 			data={'item_text':'A new list item'})
 		self.assertIn('A new list item', response.content.decode())
 		self.assertTemplateUsed(response, 'home.html')
+
+
+class ItemModeltest(TestCase):
+	def test_saving_and_retrieving_items(self):
+		first_item = Item()
+		first_item.text = 'The first (ever) list item'
+		first_item.save()
+
+		second_item = Item()
+		second_item.text = 'Item the second'
+		second_item.save()
+
+		saved_items = Item.objects.all()
+		self.assertEqual(saved_items.count(), 2)
+
+		first_saved_item = saved_items[0]
+		second_saved_item = saved_items[1]
+		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+		self.assertEqual(second_saved_item.text, 'Item the second')
+		
