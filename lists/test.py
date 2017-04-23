41d4e5800b78df6e4368ee4ffe4e5cee0e043e34
Arian Hedayati Far <arian.hedayatifar@gmail.com>
2017-04-23 12:29:10 +0430

    added one helper function to FT to make it more readable

--- lists/test.py
+++ lists/test.py
@@ -8,6 +8,11 @@ from lists.views import home_page
 
 class HomePageTest(TestCase):
 
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
 	def test_uses_home_template(self):
 		response = self.client.get('/')
 		self.assertTemplateUsed(response, 'home.html')
